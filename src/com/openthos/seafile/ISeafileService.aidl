bb390992f2e12941f5db0b637b830bd7bc0f00c2
Chen Peng <chenpeng@openthos.org>
2018-01-26 04:39:21 +0000

    OtoCloudService: Support seafile feature of preDownload and preInstall
    Signed-off-by: Chen Peng <chenpeng@openthos.org>
    Signed-off-by: Wang Zhixu <wangzhixu@openthos.org>

--- src/com/openthos/seafile/ISeafileService.aidl
+++ src/com/openthos/seafile/ISeafileService.aidl
@@ -13,4 +13,8 @@ interface ISeafileService {
     int updateSync(int userId, String libraryId, String libraryName, int isSync);
     int insertLibrary(int userId, String libraryId, String libraryName);
     String create(String text);
+    void restoreSettings(boolean wallpaper, boolean wifi, boolean email, boolean appdata,
+            boolean startupmenu, boolean browser, boolean appstore);
+    void saveSettings(boolean wallpaper, boolean wifi, boolean email, boolean appdata,
+            boolean startupmenu, boolean browser, boolean appstore);
 }
--- src/com/openthos/seafile/SeafileService.java
+++ src/com/openthos/seafile/SeafileService.java
@@ -13,16 +13,23 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.os.Handler;
 import android.os.Message;
-
+import android.content.ComponentName;
 import org.json.JSONArray;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import java.util.ArrayList;
 
 public class SeafileService extends Service {
 
     public static String mLibrary;
+    private static final String APPSTORE_PKGNAME_SEAFILE_PATH = "";
 
     private ISeafileService.Stub mBinder = new ISeafileService.Stub() {
         public void sync(String libraryid, String filePath) {
@@ -50,7 +57,8 @@ public class SeafileService extends Service {
         }
 
         public int isSync(String libraryId, String libraryName) {
-            return MainSeafileService.mConsole.queryFile(MainSeafileService.mAccount.mUserId, libraryId, libraryName);
+            return MainSeafileService.mConsole.queryFile(
+                    MainSeafileService.mAccount.mUserId, libraryId, libraryName);
         }
 
         public int updateSync(int userId, String libraryId, String libraryName, int isSync) {
@@ -64,6 +72,35 @@ public class SeafileService extends Service {
         public String create(String text) {
             return SeafileUtils.create(text);
         }
+
+        public void restoreSettings(boolean wallpaper, boolean wifi, boolean email, boolean appdata,
+                boolean startupmenu, boolean browser, boolean appstore) {
+            if (appstore) {
+                ArrayList<String> pkgNames = new ArrayList();
+                File file = new File("APPSTORE_PKGNAME_SEAFILE_PATH");
+                try {
+                    BufferedReader appReader = new BufferedReader(
+                                               new FileReader(APPSTORE_PKGNAME_SEAFILE_PATH));
+                    String line = null;
+                    while ((line = appReader.readLine()) != null) {
+                        pkgNames.add(line);
+                    }
+                    appReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                // download apps from appstore
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.openthos.appstore",
+                        "com.openthos.appstore.download.DownloadService"));
+                intent.putStringArrayListExtra("packageNames", pkgNames);
+                startService(intent);
+            }
+        }
+
+        public void saveSettings(boolean wallpaper, boolean wifi, boolean email, boolean appdata,
+                boolean startupmenu, boolean browser, boolean appstore) {}
     };
 
     @Override
