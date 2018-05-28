8acd8088612d5e4a4ddb2807169842de1ae5d8cc
Wang Zhixu <wangzhixu@openthos.org>
2018-05-28 02:10:37 +0000

    OtoCloudService: Add build rule about jsoup
    Signed-off-by: Wang Zhixu <wangzhixu@openthos.org>

--- Android.mk
+++ Android.mk
@@ -6,6 +6,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-appcompat
+LOCAL_STATIC_JAVA_LIBRARIES += jsoup
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
                    src/org/openthos/seafile/ISeafileService.aidl
@@ -18,10 +19,10 @@ LOCAL_JNI_SHARED_LIBRARIES := libkillpid
 
 LOCAL_CERTIFICATE := platform
 
-LOCAL_STATIC_JAVA_LIBRARIES += jsoup
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES  := jsoup:jsoup-1.8.1.jar
-
 include $(BUILD_PACKAGE)
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES  := jsoup:jsoup-1.8.1.jar
+include $(BUILD_MULTI_PREBUILT)
 include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := \
@@ -36,8 +37,6 @@ LOCAL_CFLAGS := \
 
 LOCAL_MODULE := libkillpid
 LOCAL_SRC_FILES := jni/android/org_openthos_seafile_Jni.cpp
-LOCAL_SHARED_LIBRARIES := \
-    liblog
-#    libcutils \
-LOCAL_MODULE_TAGS := optional
+LOCAL_SHARED_LIBRARIES := liblog
+#LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
