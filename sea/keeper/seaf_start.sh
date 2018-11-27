ae0d44219a44d06d119cd2e9e3523ab19d8544cd
elliott10 <xiaoluoyuan@163.com>
2018-11-27 12:07:49 +0000

    packages: apps: OtoCloudService: Default sync Documents and Pictures, /tmp folder use tmpfs
    Signed-off-by: LuoyuanXiao <xiaoluoyuan@163.com>

--- sea/keeper/seaf_start.sh
+++ sea/keeper/seaf_start.sh
@@ -5,9 +5,6 @@ source setenv
 
 seaf_start()
 {
-	[ -d $seafile_ddata ] || mkdir -p $seafile_ddata
-	mountpoint -q $seaDir/data || busybox mount --bind $seafile_ddata $seaDir/data
-
 	[ -d $seafile_data ] || mkdir -p $seafile_data
 
 	$proot_cmd seaf-cli init -c $conf_dir -d $seafile_data
@@ -24,15 +21,51 @@ seaf_stop()
 	$proot_cmd seaf-cli stop -c $conf_dir > /dev/null 2>&1
 }
 
+set_environment()
+{
+		[ -d tmp ] || mkdir -p tmp
+		mountpoint -q tmp && umount tmp
+		mount -t tmpfs tmpfs tmp
+		mkdir tmp/logs tmp/state
+
+		seafile_uid=`awk '/org.openthos.seafile/{print $2}' /data/system/packages.list`
+		chown -R $seafile_uid:$seafile_uid tmp
+		busybox chmod -R +x .
+
+		#/data/data/seafile
+		[ -d $seafile_ddata ] || mkdir -p $seafile_ddata
+		mountpoint -q $seaDir/data || busybox mount --bind $seafile_ddata $seaDir/data
+
+		[ -d $seafile_sdcard ] || mkdir -p $seafile_sdcard
+		[ -d $seafile_sync ] || mkdir -p $seafile_sync
+		mountpoint -q $seafile_sync || busybox mount --bind $seafile_sdcard $seafile_sync
+
+		if [ -f $conf_dir/account.conf ];then
+			source $conf_dir/account.conf
+			mountpoint -q $seafile_sync && busybox mount --bind /sdcard/Documents $seafile_sync/$user/$nDATA/Documents
+			mountpoint -q $seafile_sync && busybox mount --bind /sdcard/Pictures $seafile_sync/$user/$nDATA/Pictures
+		fi
+}
+
 do_exit()
 {
 	seaf_stop
 	echo 'seafile daemon EXIT'
+
+	umount $seafile_sync/$user/$nDATA/Documents
+	umount $seafile_sync/$user/$nDATA/Pictures
+	umount tmp
 }
 
 cd $seaDir
-busybox chmod -R 755 .
-busybox chmod -R 777 tmp
+
+if [ -f /cache/keeper.log ];then
+	echo "There's already a seafile keeper running !"
+	date
+	exit
+fi
+
+set_environment
 
 #Catch the exit SIG, "kill PID" instead of "kill -9 PID"
 trap do_exit exit
@@ -51,11 +84,25 @@ do
 	#account_sync()
 
 	if [ -f $account_conf ];then
-		[ -d $seafile_sdcard ] || mkdir -p $seafile_sdcard
-		[ -d $seafile_sync ] || mkdir -p $seafile_sync
-		mountpoint -q $seafile_sync || busybox mount --bind $seafile_sdcard $seafile_sync
 
+		grep "token=" $account_conf
+		if [ $? -eq 0 ];then
+			previous_user=$user
+			source $account_conf
+			if [ "$user"x != "$previous_user"x ];then
+				umount $seafile_sync/$previous_user/$nDATA/Documents
+				umount $seafile_sync/$previous_user/$nDATA/Pictures
+
+				[ -d $seafile_sync/$user/$nDATA/Documents ] || mkdir -p $seafile_sync/$user/$nDATA/Documents
+				[ -d $seafile_sync/$user/$nDATA/Pictures ] || mkdir -p $seafile_sync/$user/$nDATA/Pictures
+				mountpoint -q $seafile_sync && busybox mount --bind /sdcard/Documents $seafile_sync/$user/$nDATA/Documents
+				mountpoint -q $seafile_sync && busybox mount --bind /sdcard/Pictures $seafile_sync/$user/$nDATA/Pictures
+			fi
+		fi
+
+		###login check
 		$proot_cmd account_login
+
 	fi
 
 	sleep 2
