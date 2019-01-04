76e97c5d26d26ea5a0a6b777fa5fc975e9965c59
Liu Xiaoxu <liuxiaoxu@openthos.org>
2019-01-04 07:51:18 +0000

    packages: apps: OtoCloudService: Fix the issue that project quota don`t work after reboot
    Signed-off-by: Liu Xiaoxu <liuxiaoxu@openthos.org>

--- sea/keeper/seaf_start.sh
+++ sea/keeper/seaf_start.sh
@@ -38,6 +38,7 @@ set_environment()
 	mountpoint -q tmp && umount tmp
 	mount -t tmpfs tmpfs tmp
 	mkdir tmp/logs tmp/state tmp/quota
+	touch tmp/quota/quota.state
 
 	seafile_uid=`awk '/org.openthos.seafile/{print $2}' /data/system/packages.list`
 	chown -R $seafile_uid:$seafile_uid tmp
@@ -51,6 +52,10 @@ set_environment()
 	[ -d /sdcard/Documents ] || mkdir -p /sdcard/Documents
 	[ -d /sdcard/Pictures ] || mkdir -p /sdcard/Pictures
 	#mountpoint -q $seafile_sync || busybox mount --bind $seafile_sdcard $seafile_sync
+	export TMPDIR=/data/local/tmp
+	data_blk=`awk '/ \/data /{print $1}' /proc/mounts `
+	datafiles=/dev/block/${data_blk:5}
+	[  -h $data_blk ] || ln -s $datafiles /dev/
 }
 
 do_exit()
@@ -68,62 +73,6 @@ set_environment
 
 #Catch the exit SIG, "kill PID" instead of "kill -9 PID"
 trap do_exit exit
-
-while false
-do
-{
-	sleep 315
-
-	[ -f $conf_dir/account.conf ] && source $conf_dir/account.conf
-	if [ ! $token ];then
-		echo "retrieve token failed"
-		continue
-	fi
-
-	user_info=(`$proot_cmd seaf-cli user-info -c $conf_dir -s $server_url -tk $token`)
-	if [ $? -ne 0 ];then
-		echo "retrieve user-info failed"
-		continue
-	fi
-
-	unset libs_info
-	libs_info=(`$proot_cmd seaf-cli list-remote -a -c $conf_dir -s $server_url -tk $token 2>&1`)
-	if [ $? -ne 0 ];then
-		echo "retrieve libraries-info failed"
-		continue
-	fi
-	if [[ ${libs_info[@]} = *"HTTP Error 401"* ]];then
-		echo "token-invalid" > $data_info_output
-		$proot_cmd account_desync
-		continue
-	fi
-	if [ "${libs_info[1]}"x != "ID"x ];then
-		#echo "libraries-info Null"
-		continue
-	fi
-
-	lib_data=
-	mCount=3 #Retrieve first ID
-	for i in ${!libs_info[@]}
-	do
-		if [ "$i" -eq "$mCount" ];then
-			if [ "${libs_info[i]}"x = "$nDATA"x ];then
-				lib_data=${libs_info[i+2]}
-				break
-			fi
-			let "mCount+=3"
-		fi
-	done
-
-	libs_other=`expr ${user_info[5]} - $lib_data`
-	#echo ${libs_info[@]}, ${user_info[9]}
-	#echo $lib_data, $libs_other
-
-	[ -f $conf_dir/quota.conf ] && rm $conf_dir/quota.conf
-	echo "libs_other_info=$libs_other" >> $conf_dir/quota.conf
-	echo "libs_total_info=${user_info[9]}" >> $conf_dir/quota.conf
-}
-done&
 
 old_state=""
 while true
@@ -163,10 +112,8 @@ do
 		fi
 
 		if [ ! -d $seafile_sdcard/$user ];then
-				export TMPDIR=/data/local/tmp
 				data_blk=`awk '/ \/data /{print $1}' /proc/mounts `
 				datafiles=/dev/block/${data_blk:5}
-				[  -h $data_blk ] || ln -s $datafiles /dev/
 				mkdir -p $seafile_sdcard/$user
 				chattr +P -R -p 0 /data/media/0
 				chattr +P -p 666 /data/media/0/seafile/$user
@@ -177,71 +124,5 @@ do
 
 		fi
 		$proot_cmd account_sync
-	fi
-
-	if [ -f $conf_dir/quota.conf ];then
-		source $conf_dir/quota.conf
-
-		if [ ! $libs_other_info ];then
-			#echo "other libs info Null"
-			continue
-		fi
-
-		if [ ! $libs_total_info ];then
-			#echo "total libs info Null"
-			continue
-		fi
-
-		local_size=(`du -k -s $seafile_sdcard/$user/$nDATA`)
-		if [ $? -ne 0 ];then
-			echo "retrieve local-size failed"
-			continue
-		fi
-
-		local_usage=`expr ${local_size[0]} \* 1024`
-		#echo ${local_size[@]}, $local_usage
-
-		data_usage=`expr $libs_other_info + $local_usage`
-		data_rate=`awk 'BEGIN{printf "%.10f\n",('$data_usage'/'$libs_total_info')}'`
-		#echo $data_rate, $data_usage
-
-		status_info=(`$proot_cmd seaf-cli status-info -c $conf_dir`)
-		#echo ${status_info[@]}
-
-		if [ $(expr $data_rate \> 0.9) -eq 1 ];then
-			if echo "${status_info[@]}" | grep -w "auto sync disabled" &>/dev/null; then
-				echo 'seafile alreay disabled' &>/dev/null
-			else
-				#echo 'seafile will disable'
-				$proot_cmd seaf-cli disable-auto-sync -c $conf_dir
-				if [ -f $quota_info_output ];then
-					source $quota_info_output
-					if [ $quota_state != "DISABLED" ];then
-						echo quota_state=DISABLED > $quota_info_output
-					fi
-				else
-					echo quota_state=DISABLED > $quota_info_output
-				fi
-			fi
-		elif [ $(expr $data_rate \> 0.8) -eq 1 ];then
-			if echo "${status_info[@]}" | grep -w "auto sync disabled" &>/dev/null; then
-				#echo 'seafile need auto sync enable'
-				$proot_cmd seaf-cli enable-auto-sync -c $conf_dir
-			fi
-			if [ -f $quota_info_output ];then
-				source $quota_info_output
-				if [ $quota_state != "WARNING" ];then
-					echo quota_state=WARNING > $quota_info_output
-				fi
-			else
-				echo quota_state=WARNING > $quota_info_output
-			fi
-		else
-			[ -f $quota_info_output ] && rm $quota_info_output
-			if echo "${status_info[@]}" | grep -w "auto sync disabled" &>/dev/null; then
-				$proot_cmd seaf-cli enable-auto-sync -c $conf_dir
-				#echo 'seafile need auto sync enable'
-			fi
-		fi
 	fi
 done
