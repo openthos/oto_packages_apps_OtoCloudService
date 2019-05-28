a0cb72e4c2015f17d25a2aa33c204461a8ccd13d
Wang Zhixu <wangzhixu@openthos.org>
2019-05-28 02:25:43 +0000

    packages: apps: OtoCloudService: Fix the error about BaseService
    Signed-off-by: Wang Zhixu <wangzhixu@openthos.org>

--- platform-22/src/org/openthos/seafile/BaseService.java
+++ platform-22/src/org/openthos/seafile/BaseService.java
@@ -1,8 +1,5 @@
 package org.openthos.seafile;
 
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
